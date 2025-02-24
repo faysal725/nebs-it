d80d9d8c48435e42cc5daf7d675b0435fe280122
faysal725 <afaysal725@gmail.com>
2025-02-24 19:46:29 +0600

    responsive issue resolved, errors resolved

--- app/thank-you/page.jsx
+++ app/thank-you/page.jsx
@@ -4,8 +4,7 @@ import Herosection from '../ui/Herosection';
 
 export default function page() {
   return (
-    <div className=" bg-cover bg-center" 
-    style={{ backgroundImage: `url(${herosectionBg.src})` }}>
+    <div >
       <Herosection isServeyFinish={true}/>
     </div>
   )
